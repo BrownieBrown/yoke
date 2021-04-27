d4d37e7ebb3e6ffb76248d9c5884931fe3af9046
BrownieBrown <61498988+BrownieBrown@users.noreply.github.com>
2021-04-27 15:49:25 +0200

    managed to make the data loader work

--- src/main/kotlin/mbraun/yoke/util/UserDataLoader.kt
+++ src/main/kotlin/mbraun/yoke/util/UserDataLoader.kt
@@ -1,38 +1,38 @@
-//package mbraun.yoke.util
-//
-//import mbraun.yoke.model.Gender
-//import mbraun.yoke.model.Role
-//import mbraun.yoke.model.UserData
-//import mbraun.yoke.repository.UserDataRepository
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.stereotype.Component
-//import java.util.*
-//import javax.annotation.PostConstruct
-//
-//@Component
-//class UserDataLoader(@Autowired private val userDataRepository: UserDataRepository) {
-//
-//    @PostConstruct
-//    fun loadData(){
-//        userDataRepository.deleteAll()
-//        userDataRepository.save(UserData(
-//            UUID.randomUUID(),
-//            "123456",
-//            "BrownieBrown",
-//            "marco.braun2013@icloud.com",
-//            30,
-//            Gender.MALE,
-//            Role.PRIMARY_OWNER
-//        ))
-//        userDataRepository.save(UserData(
-//            UUID.randomUUID(),
-//            "i_love_marco",
-//            "SexyJTSophie",
-//            "julia.thum@gmx.de",
-//            25,
-//            Gender.FEMALE,
-//            Role.ADMIN
-//        ))
-//    }
-//
-//}
+package mbraun.yoke.util
+
+import mbraun.yoke.model.Gender
+import mbraun.yoke.model.Role
+import mbraun.yoke.model.UserData
+import mbraun.yoke.repository.UserDataRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.annotation.PostConstruct
+
+@Component
+class UserDataLoader(@Autowired private val userDataRepository: UserDataRepository) {
+
+    @PostConstruct
+    fun loadData(){
+        userDataRepository.deleteAll()
+        userDataRepository.save(UserData(
+            UUID.randomUUID(),
+            "123456",
+            "BrownieBrown",
+            "marco.braun2013@icloud.com",
+            30,
+            Gender.MALE,
+            Role.PRIMARY_OWNER
+        ))
+        userDataRepository.save(UserData(
+            UUID.randomUUID(),
+            "i_love_marco",
+            "SexyJTSophie",
+            "julia.thum@gmx.de",
+            25,
+            Gender.FEMALE,
+            Role.ADMIN
+        ))
+    }
+
+}
